6d18527ac36157250f91e2c316d9af56251e4ca1
ERIC <cheruyoterick4@gmail.com>
2023-06-14 14:17:47 +0200

    third commit

--- my-app/app/movies/api/route.ts
+++ my-app/app/movies/api/route.ts
@@ -2,14 +2,16 @@ import {NextResponse} from 'next/server';
 
 export async function GET(){
 
-    const response = await fetch('https://api.airtable.com/v0/appBDIWPFLg0qOQIl/movies ',{
+    const response = await fetch('https://api.airtable.com/v0/app6LGrkjX3FjxTwd/Movies ',{
         headers: {
-        Authorization: `Bearer  ${process.env.AIRTABLE_TOKEN} `,
+        Authorization: `Bearer ${process.env.AIRTABLE_TOKEN} `,
 
     }
 
     });
     const data = await response.json();
+    console.log('token: ', process.env.AIRTABLE_TOKEN)
+    console.log('data: ', data)
 
     return NextResponse.json(data.records);
 
