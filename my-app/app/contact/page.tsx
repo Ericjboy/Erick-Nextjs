4e634b7103438f614f01a1b66b7dcfbe18b7888a
Ericjboy <cheruyoterick4@gmail.com>
2023-06-27 19:45:57 +0200

    comment

--- my-app/app/contact/page.tsx
+++ my-app/app/contact/page.tsx
@@ -2,7 +2,7 @@
 export default function contact(){
 return(
     <main>
-        <div><p className="bg-blue">Email:cheruyoterick4@gmail.com</p>
+        <div><p className="bg-black">Email:cheruyoterick4@gmail.com</p>
         <p>Tell:7949347464</p>
         <p>Adress:Metalowiczna,09 Lublin</p> 
         <p></p></div>
